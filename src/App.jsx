1983a55519e87344eb28a388d763bdd5af0e19a3
Sk Asraful Azam <asrafulazam33@gmail.com>
2025-08-16 19:05:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import { HashRouter } from 'react-router-dom'
-import Register from './components/register';
+import Register from './components/Register';
 import Login from './components/Login';
 import Home from './pages/Home';
 import Savings from './pages/Savings';
